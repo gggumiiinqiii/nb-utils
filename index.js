7aff2e28e95c9dc13939dd50cef086bdb3e148b0
guminqi <guminqi@lvchengfuwu.com>
2023-11-15 10:32:24 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// a. index.js 入口文件
+import Format from "./src/format";
+import Validate from "./src/validate";
+import reduceRepeat from "./src/reduceRepeat";
+export { Format, Validate,reduceRepeat };
+
+
+
+
+
