b8159fa2b5fd0fb0e307399ecfc000f329d13436
gggumiiinqiii <1378827345@qq.com>
2024-01-02 19:56:23 +0800

    feat: 最长公共字符串

--- src/utils.js
+++ src/utils.js
@@ -66,3 +66,64 @@ export function isEmpty(value) {
 
   return false
 }
+/**
+ * 公共字符串 0(m*m*min(m,n))
+ */
+export function findContinuousIntersection(str1, str2) {
+  let maxLength = 0;
+  let endIndex = 0;
+
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      let k = 0;
+      while (
+        i + k < str1.length &&
+        j + k < str2.length &&
+        str1[i + k] === str2[j + k]
+      ) {
+        k++;
+      }
+
+      if (k > maxLength) {
+        maxLength = k;
+        endIndex = i + k;
+      }
+    }
+  }
+
+  return str1.substring(endIndex - maxLength, endIndex);
+}
+/**
+ * 动态规划解决最长公共字符串O(m*n)
+ * @returns 
+ */
+export function findContinuousIntersectionDP(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+
+  // 创建一个二维数组用于存储子问题的解
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+  let maxLength = 0;  // 记录最长公共子串的长度
+  let endIndex = 0;   // 记录最长公共子串在 str1 中结束的索引位置
+
+  // 填充动态规划表格
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+
+        // 如果找到更长的公共子串，则更新 maxLength 和 endIndex
+        if (dp[i][j] > maxLength) {
+          maxLength = dp[i][j];
+          endIndex = i;
+        }
+      } else {
+        dp[i][j] = 0; // 如果字符不相等，重置子问题的解为0
+      }
+    }
+  }
+
+  // 返回最长公共子串
+  return str1.substring(endIndex - maxLength, endIndex);
+}
